fed5b28db58d3a84de96f1bf8c093c4c272221c4
Amir <aemadzadeh@nvidia.com>
2017-08-17 18:13:02 -0700

    Code up to setting pixel colors.

---
+++ src/Screen.cpp
@@ -0,0 +1,21 @@
+/*
+ * Screen.cpp
+ *
+ *  Created on: Aug 17, 2017
+ *      Author: aemadzadeh
+ */
+
+#include "Screen.h"
+
+namespace particles {
+
+Screen::Screen() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Screen::~Screen() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace particles */
---
+++ src/Screen.h
@@ -0,0 +1,21 @@
+/*
+ * Screen.h
+ *
+ *  Created on: Aug 17, 2017
+ *      Author: aemadzadeh
+ */
+
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+namespace particles {
+
+class Screen {
+public:
+	Screen();
+	virtual ~Screen();
+};
+
+} /* namespace particles */
+
+#endif /* SCREEN_H_ */
